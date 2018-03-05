ba9e3489f4e299781626bf0336170eaf850b22d4
cynthia.quach <ccquach93@gmail.com>
2018-03-04 20:32:35 -0800

    configure body-parser

--- JSON APIs/todos_api/index.js
+++ JSON APIs/todos_api/index.js
@@ -1,12 +1,17 @@
-var express = require('express'),
-    app     = express(),
-    port    = process.env.PORT || 3000;
+var express     = require('express'),
+    app         = express(),
+    port        = process.env.PORT || 3000,
+    bodyParser  = require('body-parser');
 
 // dotenv Configuration
 require('dotenv').config();
 
 // Require Routes
 var todoRoutes = require('./routes/todos');
+
+// App Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     res.send("hello from root route");
