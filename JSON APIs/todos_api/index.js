ded4d4d82788090623b807dfa7cd9aa8de59eca2
cynthia.quach <ccquach93@gmail.com>
2018-03-04 17:35:54 -0800

    connect todos router and add dotenv config

--- JSON APIs/todos_api/index.js
+++ JSON APIs/todos_api/index.js
@@ -2,14 +2,19 @@ var express = require('express'),
     app     = express(),
     port    = process.env.PORT || 3000;
 
+// dotenv Configuration
+require('dotenv').config();
+
+// Require Routes
+var todoRoutes = require('./routes/todos');
+
 app.get('/', function(req, res) {
-    res.json({ message: 'hi from js object' });
+    res.send("hello from root route");
 });
 
-app.get('/happy', function(req, res) {
-    res.send(':)');
-});
+// Express Routers
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, function() {
-    console.log('Serving Todo app on Port ' + port);
+    console.log("Serving Todo app on Port " + port);
 });
