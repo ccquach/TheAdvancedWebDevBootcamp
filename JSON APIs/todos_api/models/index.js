96532ba6e5537c01f81ec5d644bd2d431c099dcd
cynthia.quach <ccquach93@gmail.com>
2018-03-04 17:03:56 -0800

    create todo model

---
+++ JSON APIs/todos_api/models/index.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+mongoose.set('debug', true);
+mongoose.Promise = global.Promise;
+const databaseUri = process.env.DB_URL || 'mongodb://127.0.0.1/todo-api';
+const databaseOptions = {
+	user: process.env.DB_USER,
+	pass: process.env.DB_PWD,
+	auth: {
+		authdb: 'admin'
+	}
+};
+mongoose.connect(databaseUri, databaseOptions)
+	.then(() => console.log(`Database connected`))
+	.catch(err => console.log(`Database connection error: ${err.message}`))
+;
+
+module.exports.Todo = require('./todo');
