cda0966549bd60363da8f60cb560cf21cd96b837
cynthia.quach <ccquach93@gmail.com>
2018-03-04 20:32:51 -0800

    define create route

--- JSON APIs/todos_api/routes/todos.js
+++ JSON APIs/todos_api/routes/todos.js
@@ -12,4 +12,14 @@ router.get('/', function(req, res) {
     });
 });
 
+router.post('/', function(req, res) {
+    db.Todo.create(req.body)
+    .then(function(newTodo) {
+        res.status(201).json(newTodo);
+    })
+    .catch(function(err) {
+        res.send(err);
+    });
+});
+
 module.exports = router;
