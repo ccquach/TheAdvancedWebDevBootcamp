6d13d3012180c826fb853fbbacdb014716e93495
cynthia.quach <ccquach93@gmail.com>
2018-03-05 21:13:42 -0800

    connect form to API

--- JSON APIs/todos_api/public/app.js
+++ JSON APIs/todos_api/public/app.js
@@ -1,15 +1,38 @@
 $(document).ready(function() {
     $.getJSON('/api/todos')
-    .then(addTodos)
+    .then(addTodos);
+
+    $('#todoInput').keypress(function(event) {
+        if(event.which === 13) {
+            createTodo();
+        }
+    });
 });
 
 function addTodos(todos) {
     //add todos to page here
     todos.forEach(function(todo) {
-        var newTodo = $('<li class="task">' + todo.name + '</li>');
-        if(todo.completed) {
-            newTodo.addClass('done');
-        }
-        $('.list').append(newTodo);
+        addTodo(todo);
+    });
+}
+
+function addTodo(todo) {
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    if(todo.completed) {
+        newTodo.addClass('done');
+    }
+    $('.list').append(newTodo);
+}
+
+function createTodo() {
+    //send request to create new todo
+    var userInput = $('#todoInput').val();
+    $.post('/api/todos', { name: userInput })
+    .then(function(newTodo) {
+        $('#todoInput').val('');
+        addTodo(newTodo);
+    })
+    .catch(function(err) {
+        console.log(err);
     });
 }
