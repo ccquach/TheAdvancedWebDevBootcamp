eadf1f38207978bc9102ff995e7a7d7d752cd889
cynthia.quach <ccquach93@gmail.com>
2018-03-05 20:52:22 -0800

    add initial AJAX call to get all todos

--- JSON APIs/todos_api/public/app.js
+++ JSON APIs/todos_api/public/app.js
@@ -1,1 +1,12 @@
-alert("hi from app.js");
+$(document).ready(function() {
+    $.getJSON('/api/todos')
+    .then(addTodos)
+});
+
+function addTodos(todos) {
+    //add todos to page here
+    todos.forEach(function(todo) {
+        var newTodo = $('<li class="task">' + todo.name + '</li>');
+        $('.list').append(newTodo);
+    });
+}
