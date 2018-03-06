0810ded0fa298c8dc0fecfd1fa06c2be67eb5b0d
cynthia.quach <ccquach93@gmail.com>
2018-03-05 20:14:08 -0800

    set up app to serve static files

--- JSON APIs/todos_api/index.js
+++ JSON APIs/todos_api/index.js
@@ -12,9 +12,11 @@ var todoRoutes = require('./routes/todos');
 // App Configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.send("hello from root route");
+    res.sendFile('index.html');
 });
 
 // Express Routers
