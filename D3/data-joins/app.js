f0cfbdefdce19e7bdbcf3e54c5ffae26f345b329
cynthia.quach <ccquach93@gmail.com>
2018-03-13 22:05:15 -0700

    D3 general update pattern

--- D3/data-joins/app.js
+++ D3/data-joins/app.js
@@ -27,6 +27,20 @@ var quotes = [
   }
 ];
 
+var newQuotes = [
+  {
+    quote: "Houston, we have a problem.",
+    movie: "Apollo 13",
+    year: 1995,
+    rating: "PG-13"
+  }, {
+    quote: "Gentlement, you can't fight in here! This is the war room!",
+    movie: "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+    year: 1964,
+    rating: "PG"
+  }
+];
+
 var colors = {
   "G": "#3cff00",
   "PG": "#f9ff00",
@@ -34,24 +48,56 @@ var colors = {
   "R": "#ff0000"
 }
 
+var listItem;
+
+// add more quotes
+var add = d3.select("#add");
+
+add.on('click', function() {
+  quotes = quotes.concat(newQuotes);
+  mergeData(addData(bindData(quotes)), bindData(quotes));
+  add.remove();
+});
+
 // enter selection
 d3.select("#quotes")
-    .style("list-style", "none")
-  .selectAll("li")
-  .data(quotes)
-  .enter()
-  .append("li")
-    .text(d => `"${d.quote}" - ${d.movie} (${d.year})`)
-    .style("margin", "20px")
-    .style("padding", "20px")
-    .style("font-size", d => d.quote.length < 25 ? "2em" : "1em")
-    .style("background-color", d => colors[d.rating])
-    .style("border-radius", "8px");
+    .style("list-style", "none");
+addData(bindData(quotes));
+
+function bindData(data) {
+  return d3.select("#quotes")
+      .selectAll("li")
+      .data(data);
+}
+
+function addData(selection) {
+  return selection
+    .enter()
+    .append("li")
+      .text(d => `"${d.quote}" - ${d.movie} (${d.year})`)
+      .style("margin", "20px")
+      .style("padding", "20px")
+      .style("font-size", d => d.quote.length < 25 ? "2em" : "1em")
+      .style("background-color", d => colors[d.rating])
+      .style("border-radius", "8px");
+}
+
+function mergeData(selection, otherSelection) {
+  selection
+    .merge(otherSelection)
+      .style("color", "#5599ff");
+}
 
 // exit selection
-// var nonRQuotes = quotes.filter(movie => movie.rating !== "R");
+var removeBtn = d3.select("#remove");
 
-// d3.selectAll("li")
-//   .data(nonRQuotes, d => d.quote)
-//   .exit()
-//   .remove();
+removeBtn.on('click', function() {
+  var nonRQuotes = quotes.filter(movie => movie.rating !== "R");
+
+  d3.selectAll("li")
+    .data(nonRQuotes, d => d.quote)
+    .exit()
+    .remove();
+
+  removeBtn.remove();
+});
