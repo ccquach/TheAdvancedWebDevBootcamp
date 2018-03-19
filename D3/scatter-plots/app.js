1bbcfdbd3a7e8ec60a1a27ed351ae2a7bdf886db
cynthia.quach <ccquach93@gmail.com>
2018-03-18 17:56:02 -0700

    D3 axes and gridlines

--- D3/scatter-plots/app.js
+++ D3/scatter-plots/app.js
@@ -1,23 +1,44 @@
 var width = 500;
 var height = 500;
-var padding = 20;
+var padding = 30;
 
 // life expectancy
 var yScale = d3.scaleLinear()
                .domain(d3.extent(birthData2011, d => d.lifeExpectancy))
 							 .range([height - padding, padding]);
+
 // births per capita
 var xScale = d3.scaleLinear()
 							 .domain(d3.extent(birthData2011, d => d.births / d.population))
 							 .range([padding, width - padding]);
+
+var xAxis = d3.axisBottom(xScale)
+							.tickSize(-height + 2 * padding)
+							.tickSizeOuter(0);
+
+var yAxis = d3.axisLeft(yScale)
+							.tickSize(-width + 2 * padding)
+							.tickSizeOuter(0);
+
 // population density
 var colorScale = d3.scaleLinear()
 									 .domain(d3.extent(birthData2011, d => d.population / d.area))
 									 .range(['lightgreen', 'black']);
+
 // births
 var radiusScale = d3.scaleLinear()
 										.domain(d3.extent(birthData2011, d => d.births))
 										.range([2, 40]);
+
+d3.select("svg")
+	.append("g")
+		.attr("transform", `translate(0, ${height - padding})`)
+		.call(xAxis);
+
+d3.select("svg")
+	.append("g")
+		.attr("transform", `translate(${padding}, 0)`)
+		.call(yAxis);
 
 d3.select("svg")
 		.attr("width", width)
@@ -30,3 +51,28 @@ d3.select("svg")
 		.attr("cy", d => yScale(d.lifeExpectancy))
 		.attr("fill", d => colorScale(d.population / d.area))
 		.attr("r", d => radiusScale(d.births));
+
+d3.select("svg")
+	.append("text")
+		.attr("x", width / 2)
+		.attr("y", height - padding)
+		.attr("dy", "1.5em")
+		.style("text-anchor", "middle")
+		.text("Births per Capita");
+
+d3.select("svg")
+	.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("x", -height / 2)
+		.attr("y", padding)
+		.attr("dy", "-1.1em")
+		.style("text-anchor", "middle")
+		.text("Life Expectancy");
+
+d3.select("svg")
+	.append("text")
+		.attr("x", width / 2)
+		.attr("y", padding)
+		.style("text-anchor", "middle")
+		.style("font-size", "1.5em")
+		.text("Data on Births by Country in 2011");
