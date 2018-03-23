fd503502e72e6c99c9b251c0d04b1e9f183d5254
cynthia.quach <ccquach93@gmail.com>
2018-03-22 21:25:18 -0700

    D3 histogram exercise add axes and axes labels

--- D3/histogram-exercise/app.js
+++ D3/histogram-exercise/app.js
@@ -1,14 +1,14 @@
 var width = 600;
 var height = 400;
 var barPadding = 1;
-var padding = 20;
+var padding = 40;
 var data = regionData.filter(mustHaveKeys);
 var minAge = d3.min(data, d => d.medianAge);
 var maxAge = d3.max(data, d => d.medianAge);
 
 var xScale =
   d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.medianAge)])
+    .domain(d3.extent(data, d => d.medianAge))
     .rangeRound([padding, width - padding]);
 
 var histogram = 
@@ -22,10 +22,42 @@ var bins = histogram(data);
 var yScale =
   d3.scaleLinear()
     .domain([0, d3.max(bins, d => d.length)])
-    .range([height, 0]);
+    .range([height - padding, padding]);
+
+var xAxis = d3.axisBottom(xScale);
+
+var yAxis = d3.axisLeft(yScale);
+
+var svg = d3.select("svg");
+
+svg
+  .append("g")
+  .attr("transform", `translate(0, ${height - padding})`)
+  .call(xAxis);
+
+svg
+  .append("g")
+  .attr("transform", `translate(${padding}, 0)`)
+  .call(yAxis);
+
+svg
+  .append("text")
+    .attr("x", width / 2)
+    .attr("y", height - padding)
+    .attr("dy", "2.2em")
+    .attr("text-anchor", "middle")
+    .text("Median Age");
+    
+svg
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("dy", "1.0em")
+    .attr("text-anchor", "middle")
+    .text("Frequency");
 
 var bars =
-  d3.select("svg")
+  svg
       .attr("width", width)
       .attr("height", height)
     .selectAll(".bar")
@@ -35,11 +67,11 @@ var bars =
       .classed("bar", true)
       .attr("x", d => xScale(d.x0))
       .attr("y", d => yScale(d.length))
+      .attr("height", d => height - yScale(d.length) - padding)
       .attr("width", d => {
         var width = xScale(d.x1) - xScale(d.x0) - barPadding;
         return width < 0 ? 0 : width;
       })
-      .attr("height", d => height - yScale(d.length))
       .attr("fill", "#0000ff");
 
 // d3.select("input")
