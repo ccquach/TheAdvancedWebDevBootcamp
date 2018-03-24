8d992643fc9e6f4e122427c4c0e4faff567659cb
cynthia.quach <ccquach93@gmail.com>
2018-03-24 12:01:29 -0700

    D3 histogram exercise refactor to eliminate code duplication

--- D3/histogram-exercise/app.js
+++ D3/histogram-exercise/app.js
@@ -3,42 +3,22 @@ var height = 600;
 var barPadding = 1;
 var padding = 80;
 var data = regionData.filter(d => d.medianAge !== null);
+var initialBinCount = 16;
 
-var xScale =
-  d3.scaleLinear()
-    .domain(d3.extent(data, d => d.medianAge))
-    .rangeRound([padding, width - padding]);
-
-var histogram = 
-  d3.histogram()
-      .domain(xScale.domain())
-      .thresholds(xScale.ticks())
-      .value(d => d.medianAge);
-
-var bins = histogram(data);
-
-var yScale =
-  d3.scaleLinear()
-    .domain([0, d3.max(bins, d => d.length)])
-    .range([height - padding, padding]);
-
-var xAxis = d3.axisBottom(xScale);
-
-var yAxis = d3.axisLeft(yScale);
-
-var svg = d3.select("svg");
-
+var svg = 
+  d3.select("svg")
+      .attr("width", width)
+      .attr("height", height);
+      
 svg
   .append("g")
     .attr("id", "xAxis")
-    .attr("transform", `translate(0, ${height - padding})`)
-    .call(xAxis);
-
+    .attr("transform", `translate(0, ${height - padding})`);
+    
 svg
   .append("g")
-    .classed("id", "yAxis")
-    .attr("transform", `translate(${padding}, 0)`)
-    .call(yAxis);
+    .attr("id", "yAxis")
+    .attr("transform", `translate(${padding}, 0)`);
 
 svg
   .append("text")
@@ -47,7 +27,7 @@ svg
     .attr("dy", padding / 2)
     .style("text-anchor", "middle")
     .text("Median Age");
-    
+
 svg
   .append("text")
     .attr("transform", "rotate(-90)")
@@ -58,22 +38,68 @@ svg
 
 svg
   .append("text")
-  .attr("id", "count")
-  .attr("x", width / 2)
-  .attr("y", height - padding)
-  .attr("dy", "4.5em")
-  .style("text-anchor", "middle")
-  .text(`Number of bins: ${bins.length}`);
+    .attr("id", "count")
+    .attr("x", width / 2)
+    .attr("y", height - padding)
+    .attr("dy", "4.5em")
+    .style("text-anchor", "middle")
+    .text(`Number of bins: ${initialBinCount}`);
 
-var bars =
-  svg
-      .attr("width", width)
-      .attr("height", height)
-    .selectAll(".bar")
-    .data(bins)
+updateRects(initialBinCount);
+
+d3.select("input")
+    .property("value", initialBinCount)
+    .on("input", function() {
+      updateRects(+d3.event.target.value);
+});
+      
+function updateRects(val) {
+  var xScale =
+    d3.scaleLinear()
+      .domain(d3.extent(data, d => d.medianAge))
+      .rangeRound([padding, width - padding]);
+
+  var ticks = xScale.ticks(val);
+  
+  var histogram = 
+    d3.histogram()
+      .domain(xScale.domain())
+      .thresholds(ticks)
+      .value(d => d.medianAge);
+  
+  var bins = histogram(data);
+  
+  var yScale =
+    d3.scaleLinear()
+      .domain([0, d3.max(bins, d => d.length)])
+      .range([height - padding, padding]);
+  
+  d3.select("#xAxis")
+      .call(
+        d3.axisBottom(xScale)
+          .tickValues(ticks)
+      );
+  
+  d3.select("#yAxis")
+      .call(d3.axisLeft(yScale));
+  
+  d3.select("#count")
+      .text(`Number of bins: ${bins.length}`);
+  
+  var bars =
+    svg
+      .selectAll(".bar")
+      .data(bins);
+
+  bars
+    .exit()
+    .remove();
+  
+  bars
     .enter()
     .append("rect")
       .classed("bar", true)
+    .merge(bars)
       .attr("x", d => xScale(d.x0))
       .attr("y", d => yScale(d.length))
       .attr("height", d => height - yScale(d.length) - padding)
@@ -82,54 +108,4 @@ var bars =
         return width < 0 ? 0 : width;
       })
       .attr("fill", "#0000ff");
-
-d3.select("input")
-    .property("min", 1)
-    .property("max", 62)
-    .property("value", xScale.ticks().length)
-    .on("input", function() {
-      var numBins = +d3.event.target.value;
-      var ticks = xScale.ticks(numBins);
-      histogram.thresholds(ticks);
-      bins = histogram(data);
-      yScale.domain([0, d3.max(bins, d => d.length)]);
-      yAxis = d3.axisLeft(yScale);
-
-      xAxis.tickValues(ticks);
-      svg
-        .select("#xAxis")
-          .attr("transform", `translate(0, ${height - padding})`)
-          .call(xAxis);
-
-      svg
-        .select("#yAxis")
-          .attr("transform", `translate(${padding}, 0)`)
-          .call(yAxis);
-
-      svg
-        .select("#count")
-        .text(`Number of bins: ${bins.length}`);
-
-      bars =
-        d3.select("svg")
-          .selectAll(".bar")
-          .data(bins);
-
-      bars
-        .exit()
-        .remove();
-
-      bars
-        .enter()
-        .append("rect")
-          .classed("bar", true)
-        .merge(bars)
-          .attr("x", d => xScale(d.x0))
-          .attr("y", d => yScale(d.length))
-          .attr("height", d => height - yScale(d.length) - padding)
-          .attr("width", d => {
-            var width = xScale(d.x1) - xScale(d.x0) - barPadding;
-            return width < 0 ? 0 : width;
-          })
-          .attr("fill", "#0000ff");
-    });
+}
