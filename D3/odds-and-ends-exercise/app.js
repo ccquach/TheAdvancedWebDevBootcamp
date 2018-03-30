38d7e76e423d3106dd0e8039dc89d395f329aeae
cynthia.quach <ccquach93@gmail.com>
2018-03-29 21:23:34 -0700

    D3 async exercise add circle elements to scatterplot based on co2 and methane emissions data

--- D3/odds-and-ends-exercise/app.js
+++ D3/odds-and-ends-exercise/app.js
@@ -13,14 +13,57 @@ d3.queue()
       methaneEmissions: +row["2000"]
     };
   })
-  .await(function(err, co2Array, methaneArray) {
+  .await(function(err, co2Res, methaneRes) {
     if (err) throw err;
 
     // get data for each country
-    var data = co2Array.map(co2 => {
-      co2.methaneEmissions = methaneArray.filter(methane => methane.countryCode === co2.countryCode)[0].methaneEmissions;
+    var data = co2Res.map(co2 => {
+      co2.methaneEmissions = methaneRes.filter(methane => methane.countryCode === co2.countryCode)[0].methaneEmissions;
       return co2;
     });
     
-    
+    // plot data
+    var width = 600;
+    var height = 600;
+    var padding = 50;
+
+    // scales
+    var xScale =
+      d3.scaleLinear()
+        .domain(d3.extent(data, d => d.co2Emissions))
+        .range([padding, width - padding]);
+
+    var yScale =
+      d3.scaleLinear()
+        .domain(d3.extent(data, d => d.methaneEmissions))
+        .range([height - padding, padding]);
+
+    // build graph
+    var svg = 
+      d3.select("svg")
+          .attr("width", width)
+          .attr("height", height);
+
+    // bind data
+    var circle =
+      svg
+        .selectAll("circle")
+        .data(data);
+
+    // remove old data
+    circle
+      .exit()
+      .remove();
+
+    // add element for new nodes
+    circle
+      .enter()
+        .append("circle")
+      .merge(circle)
+        .attr("cx", d => xScale(d.co2Emissions))
+        .attr("cy", d => yScale(d.methaneEmissions))
+        .attr("r", "25px")
+        .attr("fill", "#000")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", "0.5px");
   })
