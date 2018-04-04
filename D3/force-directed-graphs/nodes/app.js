2e8038db36305590b0971df15f4cd575c684745b
cynthia.quach <ccquach93@gmail.com>
2018-04-03 21:44:43 -0700

    D3 nodes in force-directed graphs

---
+++ D3/force-directed-graphs/nodes/app.js
@@ -0,0 +1,36 @@
+var width = 600;
+var height = 600;
+
+var nodes = [
+  { color: "red", size: 5, },
+  { color: "orange", size: 10 },
+  { color: "yellow", size: 15 },
+  { color: "green", size: 20 },
+  { color: "blue", size: 25 },
+  { color: "purple", size: 30 }
+];
+
+var svg = 
+  d3.select("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+var nodeSelection = 
+  svg
+    .selectAll("circle")
+    .data(nodes)
+    .enter()
+    .append("circle")
+      .attr("r", d => d.size)
+      .attr("fill", d => d.color);
+
+var simulation = d3.forceSimulation(nodes);
+
+simulation
+  .force("center", d3.forceCenter(width / 2, height / 2))
+  .force("nodes", d3.forceManyBody())
+  .on("tick", () => {
+    nodeSelection
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
+  })
