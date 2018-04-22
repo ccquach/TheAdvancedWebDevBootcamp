aabb36416e10faa753e66d502cd5aa9baac2b9e3
cynthia.quach <ccquach93@gmail.com>
2018-04-21 22:05:18 -0700

    React state exercise build random boxes static version

---
+++ React/random-boxes-starter/src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const Box = props => (
+  <div style={{ backgroundColor: props.color }}></div>
+);
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    let boxes = [];
+    for (let i = 0; i < 32; i++) {
+      boxes.push(this.props.allColors[i]);
+    }
+
+    this.state = {
+      boxes
+    };
+
+    setTimeout(() => {
+      const randBox = Math.floor(
+        Math.random() * 
+        this.state.boxes.length
+      );
+      const randColor = Math.floor(
+        Math.random() *
+        this.props.allColors.length
+      );
+
+      
+    }, 300);
+  }
+  
+  render() {
+    const boxes = this.state.boxes.map(color => (
+      <Box
+        key={color}
+        color={color}
+      />
+    ));
+    return (
+      <div className="App">
+        {boxes}
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+              "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
+              "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
+              "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
+              "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
+              "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+              "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
+              "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
+              "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
+              "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+              "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
+              "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+              "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+              "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
+              "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
+              "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+              "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
+              "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
+              "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
+              "Yellow","YellowGreen"]
+};
+
+export default App;
