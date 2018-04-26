2c4d0b47e6d5610117baba69d2fdfc6b527198df
cynthia.quach <ccquach93@gmail.com>
2018-04-25 21:03:26 -0700

    React guess the flag game remove Navbar component and render elements in App component

--- React/guess-the-flag/src/App.js
+++ React/guess-the-flag/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './Navbar';
 import FlagGame from './FlagGame';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <header>
+          <h1>Guess The Flag!</h1>
+        </header>
         <FlagGame />
       </div>
     );
