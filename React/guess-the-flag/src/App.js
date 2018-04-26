941680ed2d615dc47d1f5b34bf713ceec31eef3d
cynthia.quach <ccquach93@gmail.com>
2018-04-25 21:01:02 -0700

    React guess the flag move state to FlagGame component

--- React/guess-the-flag/src/App.js
+++ React/guess-the-flag/src/App.js
@@ -4,103 +4,11 @@ import Navbar from './Navbar';
 import FlagGame from './FlagGame';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      countries: [],
-      answer: {},
-      guess: '',
-      hasGuessed: false,
-      isCorrect: false
-    };
-
-    this.setupGame = this.setupGame.bind(this);
-    this.handleGuess = this.handleGuess.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-  }
-
-  componentDidMount() {
-    const countriesUrl = 'https://restcountries.eu/rest/v2/all';
-
-    fetch(countriesUrl)
-      .then(this.handleErrors)
-      .then(data => data.json())
-      .then(countries => this.setupGame(countries))
-      .catch(err => console.log(err));
-  }
-
-  handleErrors(res) {
-    if (!res.ok) throw Error(res.status);
-    return res;
-  }
-
-  setupGame(data) {
-    // get four random countries
-    const countries = [];
-    while (countries.length < 4) {
-      const randIdx = Math.floor(Math.random() * data.length);
-      const country = data[randIdx];
-      if (!countries.includes(country)) countries.push(country);
-    }
-    // select random country as answer
-    const randIdx = Math.floor(Math.random() * countries.length);
-    const answer = countries[randIdx];
-    
-    this.setState({
-      data,
-      countries,
-      answer,
-      guess: '',
-      hasGuessed: false
-    });
-  }
-
-  handleGuess(option) {
-    this.setState({
-      guess: option
-    });
-  }
-
-  handleSubmit() {
-    const hasGuessed = true;
-    const {answer, guess} = this.state;
-    if (!guess) return;
-
-    const isCorrect = guess === answer.name;
-    this.setState({
-      hasGuessed,
-      isCorrect
-    });
-  }
-
-  handleReset() {
-    const {data} = this.state;
-    this.setupGame(data);
-  }
-
   render() {
-    let view = <div className="flag-game">Loading game...</div>;
-    const {countries, answer, hasGuessed, isCorrect} = this.state;
-
-    if (countries && countries.length > 0) {
-      view = 
-        <FlagGame 
-          countries={countries} 
-          answer={answer} 
-          hasGuessed={hasGuessed}
-          isCorrect={isCorrect}
-          onGuess={this.handleGuess}
-          onSubmit={this.handleSubmit}
-          onReset={this.handleReset}
-        />
-    }
-
     return (
       <div className="App">
         <Navbar />
-        {view}
+        <FlagGame />
       </div>
     );
   }
--- React/guess-the-flag/src/FlagGame.js
+++ React/guess-the-flag/src/FlagGame.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './FlagGame.css';
 
@@ -17,12 +17,21 @@ const FlagOption = ({country, onGuess}) => (
   </div>
 )
 
+FlagOption.propTypes = {
+  country: PropTypes.object.isRequired,
+  onGuess: PropTypes.func.isRequired
+}
+
 const FlagDisplay = ({answer}) => (
   <img 
     src={answer.flag} 
     alt={`{answer.name} flag`} 
   />
 );
+
+FlagDisplay.propTypes = {
+  answer: PropTypes.object.isRequired
+}
 
 const GuessButton = ({hasGuessed, onSubmit, onReset}) => {
   const onButtonClick = hasGuessed ? onReset : onSubmit;
@@ -37,40 +46,124 @@ const GuessButton = ({hasGuessed, onSubmit, onReset}) => {
   );
 }
 
-const FlagGame = ({countries, answer, hasGuessed, isCorrect, onGuess, onSubmit, onReset}) => {
-  const flagOptions = hasGuessed ? 
-    <p>{`${isCorrect ? "Correct!" : "Incorrect! Correct Answer:"} ${answer.name}`}</p> :
-    countries.map(country => (
-    <FlagOption 
-      key={country.name} 
-      country={country}
-      onGuess={onGuess}
-    />
-  ));
-
-  return (
-    <div className="flag-game">
-      <div className="flag-options">
-        {flagOptions}
-        <GuessButton 
-          hasGuessed={hasGuessed} 
-          onSubmit={onSubmit}
-          onReset={onReset}
-        />
-      </div>
-      <FlagDisplay answer={answer} />
-    </div>
-  );
-}
-
-FlagGame.propTypes = {
-  countries: PropTypes.arrayOf(PropTypes.object).isRequired,
-  answer: PropTypes.object.isRequired,
+GuessButton.propTypes = {
   hasGuessed: PropTypes.bool.isRequired,
-  isCorrect: PropTypes.bool.isRequired,
-  onGuess: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
   onReset: PropTypes.func.isRequired
+}
+
+class FlagGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      countries: [],
+      answer: {},
+      guess: '',
+      hasGuessed: false,
+      isCorrect: false
+    };
+  
+    this.setupGame = this.setupGame.bind(this);
+    this.handleGuess = this.handleGuess.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  
+  componentDidMount() {
+    const countriesUrl = 'https://restcountries.eu/rest/v2/all';
+  
+    fetch(countriesUrl)
+      .then(this.handleErrors)
+      .then(data => data.json())
+      .then(countries => this.setupGame(countries))
+      .catch(err => console.log(err));
+  }
+  
+  handleErrors(res) {
+    if (!res.ok) throw Error(res.status);
+    return res;
+  }
+  
+  setupGame(data) {
+    // get four random countries
+    const countries = [];
+    while (countries.length < 4) {
+      const randIdx = Math.floor(Math.random() * data.length);
+      const country = data[randIdx];
+      if (!countries.includes(country)) countries.push(country);
+    }
+    // select random country as answer
+    const randIdx = Math.floor(Math.random() * countries.length);
+    const answer = countries[randIdx];
+    
+    this.setState({
+      data,
+      countries,
+      answer,
+      guess: '',
+      hasGuessed: false
+    });
+  }
+  
+  handleGuess(option) {
+    this.setState({
+      guess: option
+    });
+  }
+  
+  handleSubmit() {
+    const hasGuessed = true;
+    const {answer, guess} = this.state;
+    if (!guess) return;
+  
+    const isCorrect = guess === answer.name;
+    this.setState({
+      hasGuessed,
+      isCorrect
+    });
+  }
+  
+  handleReset() {
+    const {data} = this.state;
+    this.setupGame(data);
+  }
+
+  render() {
+    const {hasGuessed, isCorrect, countries, answer} = this.state;
+    let view = <div className="flag-game">Loading game...</div>;
+
+    if (countries && countries.length > 0) {
+      const flagOptions = hasGuessed ? 
+        <p>{`${isCorrect ? "Correct!" : "Incorrect! Correct Answer:"} ${answer.name}`}</p> :
+        countries.map(country => (
+        <FlagOption 
+          key={country.name} 
+          country={country}
+          onGuess={this.handleGuess}
+        />
+      ));
+
+      view = 
+        <div className="flag-game">
+          <div className="flag-options">
+            {flagOptions}
+            <GuessButton 
+              hasGuessed={hasGuessed} 
+              onSubmit={this.handleSubmit}
+              onReset={this.handleReset}
+            />
+          </div>
+          <FlagDisplay answer={answer} />
+        </div>
+    }
+
+    return (
+      <div className="wrapper">
+        {view}
+      </div>
+    );
+  }
 }
 
 export default FlagGame;
