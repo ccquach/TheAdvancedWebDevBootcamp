a7a93d2f8705189a16ca87c1eabd653aa6648629
cynthia.quach <ccquach93@gmail.com>
2018-04-27 11:08:45 -0700

    React fullstack todo app fetch all data from api

--- React/fullstack-todo-app/todos_frontend/src/TodoList.js
+++ React/fullstack-todo-app/todos_frontend/src/TodoList.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import './TodoList.css';
 
+const API_URL = '/api/todos';
+
 class TodoList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  componentWillMount() {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    fetch(API_URL)
+    .then(res => {
+      if (!res.ok) {
+        // server-side error
+        if (res.status >= 400 && res.status < 500) {
+          return res.json().then(data => {
+            let err = { errorMessage: data.message };
+            throw err;
+          });
+        } else {
+          // server down
+          let err = { errorMessage: 'Please try again later, server is not responding' };
+          throw err;
+        }
+      }
+      return res.json();
+    })
+    .then(todos => this.setState({todos}));
   }
 
   render() {
