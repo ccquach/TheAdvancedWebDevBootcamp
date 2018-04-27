4b253e560c6e48a84c3d10fc2a9600f1369b632d
cynthia.quach <ccquach93@gmail.com>
2018-04-27 10:34:39 -0700

    React fullstack todo app add TodoList component

---
+++ React/fullstack-todo-app/todos_frontend/src/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './TodoList.css';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Todo List</h1>
+    );
+  }
+}
+
+export default TodoList;
