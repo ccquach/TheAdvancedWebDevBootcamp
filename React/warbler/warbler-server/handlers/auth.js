174344124f223064b83bdce1faaa6f5db35a8773
cynthia.quach <ccquach93@gmail.com>
2018-05-14 15:00:50 -0700

    add signin route

--- React/warbler/warbler-server/handlers/auth.js
+++ React/warbler/warbler-server/handlers/auth.js
@@ -6,9 +6,9 @@ exports.signup = async function(req, res, next) {
     let user = await db.User.create(req.body);
     let { id, username, profileImageUrl } = user;
     let token = jwt.sign({
-      id,
-      username,
-      profileImageUrl
+        id,
+        username,
+        profileImageUrl
       }, 
       process.env.SECRET_KEY
     );
@@ -29,6 +29,38 @@ exports.signup = async function(req, res, next) {
   }
 }
 
-exports.signin = function() {
-
+exports.signin = async function(req, res, next) {
+  try {
+    let user = await db.User.findOne({
+      email: req.body.email
+    });
+    let { id, username, profileImageUrl } = user;
+    let isMatch = await user.comparePassword(req.body.password);
+    if (isMatch) {
+      let token = jwt.sign({
+          id,
+          username,
+          profileImageUrl
+        },
+        process.env.SECRET_KEY
+      );
+      return res.status(200).json({
+        id,
+        username,
+        profileImageUrl,
+        token
+      });
+    } else {
+      return next({
+        status: 400,
+        message: 'Invalid email/password.'
+      });
+    }
+  } catch (err) {
+    return next({
+      status: 400,
+      message: 'Invalid email/password.'
+    });
+  }
+  
 }
