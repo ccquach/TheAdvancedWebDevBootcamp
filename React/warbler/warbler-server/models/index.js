00bc4c45a207b5ef7e9850120fb5ed539a93b51a
cynthia.quach <ccquach93@gmail.com>
2018-05-14 12:13:58 -0700

    add user model with password hashing

---
+++ React/warbler/warbler-server/models/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+mongoose.set('debug', true);
+mongoose.Promise = global.Promise;
+
+const databaseUri = process.env.DB_URL || 'mongodb://127.0.0.1/warbler';
+const databaseOptions = {
+  user: process.env.DB_USER,
+  pass: process.env.DB_PWD,
+  auth: {
+    authdb: 'admin'
+  },
+  keepAlive: true,
+  useMongoClient: true
+};
+
+mongoose.connect(databaseUri, databaseOptions)
+  .then(() => console.log(`Database connected`))
+  .catch(err => console.log(`Database connection error: ${err.message}`))
+;
