b27e8edc1e710127b88d0b5d9782da1b033b421d
cynthia.quach <ccquach93@gmail.com>
2018-05-15 21:09:47 -0700

    warbler add get and delete message routes

--- React/warbler/warbler-server/index.js
+++ React/warbler/warbler-server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const errorHandler = require('./handlers/error');
+const db = require('./models');
 const authRoutes = require('./routes/auth');
 const messagesRoutes = require('./routes/messages');
 const { loginRequired, ensureCorrectUser } = require('./middleware/auth');
@@ -21,6 +22,20 @@ app.use(
   ensureCorrectUser, 
   messagesRoutes
 );
+
+app.get('/api/messages', loginRequired, async function(req, res, next) {
+  try {
+    let messages = await db.Message.find()
+      .sort({ createdAt: 'desc' })
+      .populate('user', {
+        username: true,
+        profileImageUrl: true
+      });
+    return res.status(200).json(messages);
+  } catch (err) {
+    return next(err);
+  }
+});
 
 app.use(function(req, res, next) {
   let err = new Error('Not Found');
