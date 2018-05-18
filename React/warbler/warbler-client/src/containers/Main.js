6cad288b166a9d3bec8c8a95a65b7b10e588967d
cynthia.quach <ccquach93@gmail.com>
2018-05-17 17:48:16 -0700

    warbler add auth form

--- React/warbler/warbler-client/src/containers/Main.js
+++ React/warbler/warbler-client/src/containers/Main.js
@@ -2,16 +2,48 @@ import React from 'react';
 import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Homepage from '../components/Homepage';
+import AuthForm from '../components/AuthForm';
+import { authUser } from '../store/actions/auth';
 
 const Main = props => {
+  const { authUser } = props;
   return (
     <div className="container">
       <Switch>
-        <Route exact path="/" render={props => <Homepage { ...props } />} />
+        <Route exact path="/" render={props => <Homepage {...props} />} />
+        <Route
+          exact
+          path="/signin"
+          render={props => {
+            return (
+              <AuthForm
+                onAuth={authUser}
+                buttonText="Log in"
+                heading="Welcome Back."
+                {...props}
+              />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={props => {
+            return (
+              <AuthForm
+                onAuth={authUser}
+                signUp
+                buttonText="Sign me up!"
+                heading="Join Warbler today."
+                {...props}
+              />
+            );
+          }}
+        />
       </Switch>
     </div>
   );
-}
+};
 
 function mapStateToProps(state) {
   return {
@@ -19,4 +51,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default withRouter(connect(mapStateToProps, null)(Main));
+export default withRouter(connect(mapStateToProps, { authUser })(Main));
