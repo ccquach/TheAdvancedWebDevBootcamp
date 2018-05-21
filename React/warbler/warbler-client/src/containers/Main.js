1b5ced328b4a40044d37f6d5ac69c714fcb61fa4
cynthia.quach <ccquach93@gmail.com>
2018-05-21 10:48:06 -0700

    warbler add withAuth higher order component to validate user is logged in before allowing access to messages actions

--- React/warbler/warbler-client/src/containers/Main.js
+++ React/warbler/warbler-client/src/containers/Main.js
@@ -5,6 +5,8 @@ import Homepage from '../components/Homepage';
 import AuthForm from '../components/AuthForm';
 import { authUser } from '../store/actions/auth';
 import { removeError } from '../store/actions/errors';
+import withAuth from '../hocs/withAuth';
+import MessageForm from './MessageForm';
 
 const Main = props => {
   const { currentUser, authUser, errors, removeError } = props;
@@ -48,6 +50,10 @@ const Main = props => {
               />
             );
           }}
+        />
+        <Route
+          path="/users/:id/messages/new"
+          component={withAuth(MessageForm)}
         />
       </Switch>
     </div>
